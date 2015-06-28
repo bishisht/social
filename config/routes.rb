5d887bab45f3fc8c4427bc1edc77da42fa64ef52
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-06-28 17:17:49 +0545

    added and configured devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'site/index'
+  get 'site/help'
+  get 'site/about'
+  get 'site/faq'
+  root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
